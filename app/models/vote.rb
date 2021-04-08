0a1ecc7deb3e1325e20bc01dad399b62477e4cbd
JohnHernCode <besthostandseoinfo@gmail.com>
2021-04-08 13:14:23 -0600

    Add table and associations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
-
 class Vote < ApplicationRecord
+  belongs_to :user, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :article, foreign_key: 'article_id', class_name: 'Article'
 end
---
+++ db/migrate/20210408190351_article_categories.rb
@@ -0,0 +1,10 @@
+class ArticleCategories < ActiveRecord::Migration[6.1]
+  def change
+    create_table :article_categories do |t|
+      t.integer :CategoryId
+      t.integer :ArticleId
+
+      t.timestamps
+    end
+  end
+end
