60f22af57758a4f1985fc1b73847b373977aa6b5
JohnHernCode <besthostandseoinfo@gmail.com>
2021-04-16 11:32:23 -0600

    Fix route in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,4 +120,4 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
-Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+Rails.application.routes.default_url_options[:host] = 'localhost'
