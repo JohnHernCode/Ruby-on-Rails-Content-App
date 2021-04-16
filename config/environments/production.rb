0d313b64fcebf78f344a05de68d34e813d4c53bb
JohnHernCode <besthostandseoinfo@gmail.com>
2021-04-16 11:35:22 -0600

    Remove Production Environment line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,4 +120,3 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
-Rails.application.routes.default_url_options[:host] = 'localhost'
