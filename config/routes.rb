2d91df698f3dc4932d371da996c07a01bf74e1b7
JohnHernCode <besthostandseoinfo@gmail.com>
2021-04-17 08:06:52 -0600

    Fix Rubocop in Routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  
-  root to: "articles#index"
+  root to: 'articles#index'
 
   get '/signup', to: 'users#new'
   resources :users, only: [:new, :show, :create]
   resources :categories
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :articles do
     resources :votes, only: %i[create destroy]
